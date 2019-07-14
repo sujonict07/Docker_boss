1df212268e0d199f9aba15930a0120bbea4aa868
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2019-07-14 10:56:40 +0600

    install latest version of docker-compose

---
+++ install_docker_compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
+sudo docker-compose --version
